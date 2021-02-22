d835abfe411e894253baa171a6a2c49bc579c241
tgeb <tom.gebhardt@informatik.uni-rostock.de>
2021-02-22 20:47:26 +0100

    Update simpleMerge.php

--- bives/simpleMerge.php
+++ bives/simpleMerge.php
@@ -80,7 +80,7 @@ if (isset($f1) && !empty($f2) && isset($f2) && !empty($f2) && !isset($job)) {
 	if (!file_exists($storage) ) echo "STORAGE does not exist " . $storage;
 	else if (!file_exists($storage . '/' . $job) ) echo "ID does not exist " . $storage;
 	else if (!file_exists($storage . '/' . $job . '/' . $getFile)) echo "FILE DOESNT EXIST " . $storage . '/' . $job . '/' . $getFile;
-	echo "  FAILED! ---> getFile:" . $getFile . " job: " . $job;
+	echo "  FAILED! ---> getFile:" . $getFile . " job: " . $job . " updateID: 1";
 }
 
 function callBives($bivesJob, $saveMerge, $BIVES, $storage, $job)
